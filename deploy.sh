a97e20c34e46d77c33be70baf4605bb4d83eb8a4
owlran <owlran1088@gmail.com>
2021-01-19 23:22:27 +0800

    add deploy github page script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GTI_REPORT_URL=$(git config remote.origin.url)
+
+mkdir .deploy
+cp -R ./dist/* .deploy
+cd .deploy
+git init .
+git remote add github $GTI_REPORT_URL
+git checkout -b gb-pages
+git add .
+git commit -m "Static site deploy"
+git push github gh-pages --force
+cd ..
+# rm -rf .deploy
