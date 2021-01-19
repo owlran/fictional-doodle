d58fb23e62adb5f84be54ff407cc1b7f3d4105d0
owlran <owlran1088@gmail.com>
2021-01-20 00:31:44 +0800

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ cp -R ./dist/* .deploy
 cd .deploy
 git init .
 git remote add github $GTI_REPORT_URL
-git checkout -b gb-pages
+git checkout -b gh-pages
 git add .
 git commit -m "Static site deploy"
-git push github HEAD:refs/for/gh-pages --force
+git push github gh-pages -f
 cd ..
 rm -rf .deploy
