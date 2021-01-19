dfa729544af037e8681ede7854510ac9c3b867d3
owlran <owlran1088@gmail.com>
2021-01-19 23:25:21 +0800

    update deploy flow

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git remote add github $GTI_REPORT_URL
 git checkout -b gb-pages
 git add .
 git commit -m "Static site deploy"
-git push github gh-pages --force
+git push github HEAD:refs/for/gh-pages --force
 cd ..
 # rm -rf .deploy
